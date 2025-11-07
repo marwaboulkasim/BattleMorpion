03174637c5053c5a858970f232465f6726af8f33
samyachd <achd.samy@gmail.com>
2025-11-07 10:32:00 +0100

    premier push

---
+++ frontend/script.js
@@ -0,0 +1,51 @@
+const urlAPI = "http://127.0.0.1:8000/play"
+
+const grid = [
+    ["","",""],
+    ["","",""],
+    ["","",""],
+]
+
+const gridHTML = document.querySelector("#grid")
+
+function viewGrid() {
+    gridHTML.innerHTML = ""
+    for (let ligne of grid) {
+        for (let cell of ligne) {
+            const cellHTML = document.createElement("div")
+            cellHTML.classList.add("cell")
+            cellHTML.textContent = cell
+            gridHTML.appendChild(cellHTML)
+        }
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  viewGrid(); // affiche la grille vide tout de suite
+});
+
+let joueur = "X"
+
+document.querySelector("#play").addEventListener(
+    "click", e => {
+        /// envoyer la grid à l'API
+        fetch(urlAPI, {
+            method: "POST",
+            body: JSON.stringify(grid),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }) /// recuperer les coordonée du coup joué
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            /// mettre à jour la grid
+            grid[data[1]][data[0]] = joueur
+            /// afficher la grid
+            viewGrid()
+            if(joueur === "X") {
+                joueur = "O"
+            }else{ joueur = "X"}
+        })
+        console.log("Le joueur ",{joueur}," a joué")
+    })
